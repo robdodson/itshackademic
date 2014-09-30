9813f01ea22fe66b5b4bb83d1648869abc18a4fe
Jake Albaugh <jake.albaugh@gmail.com>
2014-09-29 20:43:51 -0500

    FAQ and Resources Base
    Implemented jQuery approach for FAQs.
    
    Implemented responsive base for resources. Currently sets px-value height for each resource in column mode.
    
    Accompanied by some small style tweaks.

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,7 +1,14 @@
+// extremely unsophisticated retina image swap for badge
 var retina = window.devicePixelRatio > 1;
 if (retina) {
   var src = document.getElementById('badge').getAttribute('src');
   document.getElementById('badge').setAttribute('src', src.replace('.png', '@2x.png'));
 }
 
+// load stellar
 $(document).stellar();
+
+// faq toggling, allows many to be open at once
+$('#faq label').click(function() {
+  $(this).parents('li').toggleClass('open');
+});
